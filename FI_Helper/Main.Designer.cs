88c3da3f48f11808130a22d1a1473972fbe7eae7
Arthur.B <48888351+Sehyn@users.noreply.github.com>
2021-07-10 20:08:29 +0200

    Add files via upload
    PS CODE IS DOG SHIT IT WAS MADE FOR ME INITIALLY IF IT MIGHT HELP YOU WELL GG

---
+++ FI_Helper/Main.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace FI_Helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ChkRyzenMaster = new System.Windows.Forms.CheckBox();
+            this.ChkGForceExp = new System.Windows.Forms.CheckBox();
+            this.ChkLogitechGhub = new System.Windows.Forms.CheckBox();
+            this.ChkWinrar = new System.Windows.Forms.CheckBox();
+            this.ChkVS = new System.Windows.Forms.CheckBox();
+            this.ChkAIO = new System.Windows.Forms.CheckBox();
+            this.ChkDiscord = new System.Windows.Forms.CheckBox();
+            this.ChkSteam = new System.Windows.Forms.CheckBox();
+            this.ChkOrigin = new System.Windows.Forms.CheckBox();
+            this.ChkOperaGx = new System.Windows.Forms.CheckBox();
+            this.ChkBattleNet = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ChkDefenderCtrl = new System.Windows.Forms.CheckBox();
+            this.ChkShareX = new System.Windows.Forms.CheckBox();
+            this.BtnDownload = new System.Windows.Forms.Button();
+            this.BarDLProgress = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblStatus = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ChkRyzenMaster);
+            this.groupBox1.Controls.Add(this.ChkGForceExp);
+            this.groupBox1.Controls.Add(this.ChkLogitechGhub);
+            this.groupBox1.Controls.Add(this.ChkWinrar);
+            this.groupBox1.Controls.Add(this.ChkVS);
+            this.groupBox1.Controls.Add(this.ChkAIO);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(148, 161);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "System Utilities:";
+            // 
+            // ChkRyzenMaster
+            // 
+            this.ChkRyzenMaster.AutoSize = true;
+            this.ChkRyzenMaster.Location = new System.Drawing.Point(6, 134);
+            this.ChkRyzenMaster.Name = "ChkRyzenMaster";
+            this.ChkRyzenMaster.Size = new System.Drawing.Size(118, 17);
+            this.ChkRyzenMaster.TabIndex = 10;
+            this.ChkRyzenMaster.Text = "AMD Ryzen Master";
+            this.ChkRyzenMaster.UseVisualStyleBackColor = true;
+            // 
+            // ChkGForceExp
+            // 
+            this.ChkGForceExp.AutoSize = true;
+            this.ChkGForceExp.Location = new System.Drawing.Point(6, 111);
+            this.ChkGForceExp.Name = "ChkGForceExp";
+            this.ChkGForceExp.Size = new System.Drawing.Size(120, 17);
+            this.ChkGForceExp.TabIndex = 9;
+            this.ChkGForceExp.Text = "Geforce Experience";
+            this.ChkGForceExp.UseVisualStyleBackColor = true;
+            // 
+            // ChkLogitechGhub
+            // 
+            this.ChkLogitechGhub.AutoSize = true;
+            this.ChkLogitechGhub.Location = new System.Drawing.Point(6, 65);
+            this.ChkLogitechGhub.Name = "ChkLogitechGhub";
+            this.ChkLogitechGhub.Size = new System.Drawing.Size(104, 17);
+            this.ChkLogitechGhub.TabIndex = 8;
+            this.ChkLogitechGhub.Text = "Logitech G HUB";
+            this.ChkLogitechGhub.UseVisualStyleBackColor = true;
+            // 
+            // ChkWinrar
+            // 
+            this.ChkWinrar.AutoSize = true;
+            this.ChkWinrar.Location = new System.Drawing.Point(6, 88);
+            this.ChkWinrar.Name = "ChkWinrar";
+            this.ChkWinrar.Size = new System.Drawing.Size(68, 17);
+            this.ChkWinrar.TabIndex = 5;
+            this.ChkWinrar.Text = "WinRAR";
+            this.ChkWinrar.UseVisualStyleBackColor = true;
+            // 
+            // ChkVS
+            // 
+            this.ChkVS.AutoSize = true;
+            this.ChkVS.Location = new System.Drawing.Point(6, 42);
+            this.ChkVS.Name = "ChkVS";
+            this.ChkVS.Size = new System.Drawing.Size(141, 17);
+            this.ChkVS.TabIndex = 1;
+            this.ChkVS.Text = "Visual Studio Community";
+            this.ChkVS.UseVisualStyleBackColor = true;
+            // 
+            // ChkAIO
+            // 
+            this.ChkAIO.AutoSize = true;
+            this.ChkAIO.Location = new System.Drawing.Point(6, 19);
+            this.ChkAIO.Name = "ChkAIO";
+            this.ChkAIO.Size = new System.Drawing.Size(97, 17);
+            this.ChkAIO.TabIndex = 0;
+            this.ChkAIO.Text = "AIO - Runtimes";
+            this.ChkAIO.UseVisualStyleBackColor = true;
+            // 
+            // ChkDiscord
+            // 
+            this.ChkDiscord.AutoSize = true;
+            this.ChkDiscord.Location = new System.Drawing.Point(6, 19);
+            this.ChkDiscord.Name = "ChkDiscord";
+            this.ChkDiscord.Size = new System.Drawing.Size(62, 17);
+            this.ChkDiscord.TabIndex = 2;
+            this.ChkDiscord.Text = "Discord";
+            this.ChkDiscord.UseVisualStyleBackColor = true;
+            // 
+            // ChkSteam
+            // 
+            this.ChkSteam.AutoSize = true;
+            this.ChkSteam.Location = new System.Drawing.Point(7, 19);
+            this.ChkSteam.Name = "ChkSteam";
+            this.ChkSteam.Size = new System.Drawing.Size(56, 17);
+            this.ChkSteam.TabIndex = 3;
+            this.ChkSteam.Text = "Steam";
+            this.ChkSteam.UseVisualStyleBackColor = true;
+            // 
+            // ChkOrigin
+            // 
+            this.ChkOrigin.AutoSize = true;
+            this.ChkOrigin.Location = new System.Drawing.Point(7, 42);
+            this.ChkOrigin.Name = "ChkOrigin";
+            this.ChkOrigin.Size = new System.Drawing.Size(53, 17);
+            this.ChkOrigin.TabIndex = 4;
+            this.ChkOrigin.Text = "Origin";
+            this.ChkOrigin.UseVisualStyleBackColor = true;
+            // 
+            // ChkOperaGx
+            // 
+            this.ChkOperaGx.AutoSize = true;
+            this.ChkOperaGx.Location = new System.Drawing.Point(6, 42);
+            this.ChkOperaGx.Name = "ChkOperaGx";
+            this.ChkOperaGx.Size = new System.Drawing.Size(73, 17);
+            this.ChkOperaGx.TabIndex = 6;
+            this.ChkOperaGx.Text = "Opera GX";
+            this.ChkOperaGx.UseVisualStyleBackColor = true;
+            // 
+            // ChkBattleNet
+            // 
+            this.ChkBattleNet.AutoSize = true;
+            this.ChkBattleNet.Location = new System.Drawing.Point(7, 65);
+            this.ChkBattleNet.Name = "ChkBattleNet";
+            this.ChkBattleNet.Size = new System.Drawing.Size(73, 17);
+            this.ChkBattleNet.TabIndex = 7;
+            this.ChkBattleNet.Text = "Battle Net";
+            this.ChkBattleNet.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ChkBattleNet);
+            this.groupBox2.Controls.Add(this.ChkSteam);
+            this.groupBox2.Controls.Add(this.ChkOrigin);
+            this.groupBox2.Location = new System.Drawing.Point(167, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(148, 161);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Game Launchers:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ChkDefenderCtrl);
+            this.groupBox3.Controls.Add(this.ChkShareX);
+            this.groupBox3.Controls.Add(this.ChkDiscord);
+            this.groupBox3.Controls.Add(this.ChkOperaGx);
+            this.groupBox3.Location = new System.Drawing.Point(321, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(148, 161);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Additional:";
+            // 
+            // ChkDefenderCtrl
+            // 
+            this.ChkDefenderCtrl.AutoSize = true;
+            this.ChkDefenderCtrl.Location = new System.Drawing.Point(6, 88);
+            this.ChkDefenderCtrl.Name = "ChkDefenderCtrl";
+            this.ChkDefenderCtrl.Size = new System.Drawing.Size(106, 17);
+            this.ChkDefenderCtrl.TabIndex = 8;
+            this.ChkDefenderCtrl.Text = "Defender Control";
+            this.ChkDefenderCtrl.UseVisualStyleBackColor = true;
+            // 
+            // ChkShareX
+            // 
+            this.ChkShareX.AutoSize = true;
+            this.ChkShareX.Location = new System.Drawing.Point(6, 65);
+            this.ChkShareX.Name = "ChkShareX";
+            this.ChkShareX.Size = new System.Drawing.Size(61, 17);
+            this.ChkShareX.TabIndex = 7;
+            this.ChkShareX.Text = "ShareX";
+            this.ChkShareX.UseVisualStyleBackColor = true;
+            // 
+            // BtnDownload
+            // 
+            this.BtnDownload.Location = new System.Drawing.Point(13, 180);
+            this.BtnDownload.Name = "BtnDownload";
+            this.BtnDownload.Size = new System.Drawing.Size(455, 23);
+            this.BtnDownload.TabIndex = 3;
+            this.BtnDownload.Text = "Download";
+            this.BtnDownload.UseVisualStyleBackColor = true;
+            this.BtnDownload.Click += new System.EventHandler(this.BtnDownload_Click);
+            // 
+            // BarDLProgress
+            // 
+            this.BarDLProgress.Location = new System.Drawing.Point(13, 209);
+            this.BarDLProgress.Name = "BarDLProgress";
+            this.BarDLProgress.Size = new System.Drawing.Size(455, 23);
+            this.BarDLProgress.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Status:";
+            // 
+            // LblStatus
+            // 
+            this.LblStatus.AutoSize = true;
+            this.LblStatus.ForeColor = System.Drawing.Color.Orange;
+            this.LblStatus.Location = new System.Drawing.Point(49, 235);
+            this.LblStatus.Name = "LblStatus";
+            this.LblStatus.Size = new System.Drawing.Size(46, 13);
+            this.LblStatus.TabIndex = 6;
+            this.LblStatus.Text = "waiting..";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(199, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "fluve.me/helper";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Start \"Install.bat\" to batch install when all files are downloaded";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 268);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BarDLProgress);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BtnDownload);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Fresh Install - [#] 0.1 [#]";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox ChkRyzenMaster;
+        private System.Windows.Forms.CheckBox ChkGForceExp;
+        private System.Windows.Forms.CheckBox ChkLogitechGhub;
+        private System.Windows.Forms.CheckBox ChkWinrar;
+        private System.Windows.Forms.CheckBox ChkVS;
+        private System.Windows.Forms.CheckBox ChkAIO;
+        private System.Windows.Forms.CheckBox ChkDiscord;
+        private System.Windows.Forms.CheckBox ChkSteam;
+        private System.Windows.Forms.CheckBox ChkOrigin;
+        private System.Windows.Forms.CheckBox ChkOperaGx;
+        private System.Windows.Forms.CheckBox ChkBattleNet;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox ChkShareX;
+        private System.Windows.Forms.Button BtnDownload;
+        private System.Windows.Forms.ProgressBar BarDLProgress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox ChkDefenderCtrl;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FI_Helper/Main.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FI_Helper
+{
+    public partial class Form1 : Form
+    {
+        private string DLStatus ="";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Fresh Install - [#] WAITING [#]";
+            WebClient webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/Installer.txt"), "Installer.bat");
+            DLStatus = "Waiting..";
+        }
+
+        private  void BtnDownload_Click(object sender, EventArgs e)
+        {
+            BtnDownload.Enabled = false;
+            BtnDownload.Text = "Please wait..";
+            BtnDownload.Visible = false;
+
+
+
+            var client = new WebClient();
+
+            if (ChkAIO.Checked == true)
+            {
+                this.DLStatus = "All In One Runtimes download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/AIO_INSTALLER.exe"), "AIO.exe");
+            }
+            if (ChkVS.Checked == true)
+            {
+                this.DLStatus = "Visual Studio download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/vs_Community_INSTALLER.exe"), "VS_Community.exe");
+            }
+            if (ChkLogitechGhub.Checked == true)
+            {
+                this.DLStatus = "Logitech G HUB download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/LGHUB_INSTALLER.exe"), "LogitechGHUB.exe");
+            }
+            if (ChkWinrar.Checked == true)
+            {
+                this.DLStatus = "WinRAR download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/WINRAR_INSTALLER.exe"), "WinRAR.exe");
+            }
+            if (ChkGForceExp.Checked == true)
+            {
+                this.DLStatus = "Geforce Experience download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/GFORCE_INSTALLER.exe"), "GeForceExperience.exe");
+            }
+            if (ChkRyzenMaster.Checked == true)
+            {
+                this.DLStatus = "AMD Ryzen Master download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/AMD-Ryzen-Master_INSTALLER.exe"), "Amd_Ryzen_Master.exe");
+            }
+            if (ChkSteam.Checked == true)
+            {
+                this.DLStatus = "Steam download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/STEAM_INSTALLER.exe"), "Steam.exe");
+            }
+            if (ChkOrigin.Checked == true)
+            {
+                this.DLStatus = "Origin download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/ORIGIN_INSTALLER.exe"), "Origin.exe");
+
+            }
+            if (ChkBattleNet.Checked == true)
+            {
+                this.DLStatus = "Battle Net download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/BATTLENET_INSTALLER.exe"), "BattleNet.exe");
+            }
+            if (ChkDiscord.Checked == true)
+            {
+                this.DLStatus = "Discord download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/DISCORD_INSTALLER.exe"), "DISCORD.exe");
+            }
+            if (ChkOperaGx.Checked == true)
+            {
+                this.DLStatus = "Opera GX download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/OperaGX_INSTALLER.exe"), "OperaGX.exe");
+            }
+            if (ChkShareX.Checked == true)
+            {
+                this.DLStatus = "ShareX download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/SHAREX_INSTALLER.exe"), "ShareX.exe");
+            }
+            if (ChkDefenderCtrl.Checked == true)
+            {
+                this.DLStatus = "Defender Control download completed.";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("https://dl.nextadvantage.xyz/Helper/DEFENDER_CONTROL.exe"), "Defender_Control.exe");
+            }
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+            LblStatus.Text = "Downloaded " + e.BytesReceived + " of " + e.TotalBytesToReceive;
+            BarDLProgress.Value = int.Parse(Math.Truncate(percentage).ToString());
+           
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            
+            int i = groupBox1.Controls.OfType<CheckBox>().Where(c => c.Checked).Count() + groupBox2.Controls.OfType<CheckBox>().Where(c => c.Checked).Count() + groupBox3.Controls.OfType<CheckBox>().Where(c => c.Checked).Count();
+            if(i > 1)
+            {
+                LblStatus.Text = (i.ToString() + " files were succesfully downloaded.");
+
+            }
+            else
+            {
+                LblStatus.Text = DLStatus;
+
+            }
+        }
+
+
+    }
+}
